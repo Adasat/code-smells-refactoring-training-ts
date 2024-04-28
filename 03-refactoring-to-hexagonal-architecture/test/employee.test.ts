eb15fec60f1a1e78632034def1c7577b9788e829
Daniel Ramos <danielramosacosta1@gmail.com>
2024-04-28 11:53:33 +0200

    migrate to vitest

--- 03-refactoring-to-hexagonal-architecture/test/employee.test.ts
+++ 03-refactoring-to-hexagonal-architecture/test/employee.test.ts
@@ -1,9 +1,9 @@
-import {Employee} from "../src/Employee";
-import {OurDate} from "../src/OurDate";
+import { describe, it, expect } from "vitest";
+import { Employee } from "../src/Employee";
+import { OurDate } from "../src/OurDate";
 
-describe('Employee', () => {
-
-  it('birthday', () => {
+describe("Employee", () => {
+  it("birthday", () => {
     const employee = new Employee("foo", "bar", "1990/01/31", "a@b.c");
 
     expect(employee.isBirthday(new OurDate("2008/01/30"))).toBeFalsy();

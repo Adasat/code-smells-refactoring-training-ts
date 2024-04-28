bd7847888469bef16fc197c256fbc571fc8ee349
Daniel Ramos <danielramosacosta1@gmail.com>
2024-04-28 11:43:54 +0200

    migrate to vitest

--- 02-refactoring-video-store/src/Movie.ts
+++ 02-refactoring-video-store/src/Movie.ts
@@ -1,25 +1,25 @@
 export class Movie {
-    public static CHILDREN: number = 2;
-    public static REGULAR: number = 0;
-    public static NEW_RELEASE: number = 1;
+  public static CHILDREN: number = 2;
+  public static REGULAR: number = 0;
+  public static NEW_RELEASE: number = 1;
 
-    private readonly _title: string;
-    private _priceCode: number;
+  private readonly _title: string;
+  private _priceCode: number;
 
-    constructor(title: string, priceCode: number) {
-        this._title = title;
-        this._priceCode = priceCode;
-    }
+  constructor(title: string, priceCode: number) {
+    this._title = title;
+    this._priceCode = priceCode;
+  }
 
-    getPriceCode(): number {
-        return this._priceCode;
-    }
+  getPriceCode(): number {
+    return this._priceCode;
+  }
 
-    setPriceCode(code: number): void {
-        this._priceCode = code;
-    }
+  setPriceCode(code: number): void {
+    this._priceCode = code;
+  }
 
-    getTitle(): string {
-        return this._title;
-    }
+  getTitle(): string {
+    return this._title;
+  }
 }

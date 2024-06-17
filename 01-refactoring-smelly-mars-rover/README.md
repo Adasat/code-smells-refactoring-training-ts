e169f8687776e3a26d23fcee03c30aa009cf6c38
Adasat <pbonbol@gmail.com>
2024-06-17 19:32:39 +0100

    - Process Method - Separated Complex Boolean Expression

--- 01-refactoring-smelly-mars-rover/README.md
+++ 01-refactoring-smelly-mars-rover/README.md
@@ -5,3 +5,13 @@ Smelly Mars Rover code to practice refactoring.
 
 We'll use it to learn to recognize some code smells
 and to fix them applying some useful refactorings.
+
+
+#### Smells
+1. Long Method
+2. Long Class
+3. Primitive Obsession
+4. Data Clumb
+5. Boolean complex expression
+6. Rotate Rover
+
--- 01-refactoring-smelly-mars-rover/src/Rover.ts
+++ 01-refactoring-smelly-mars-rover/src/Rover.ts
@@ -13,51 +13,82 @@ export class Rover {
     for (let i = 0; i < commandsSequence.length; ++i) {
       const command = commandsSequence.substring(i, i + 1);
 
-      if (command === "l" || command === "r") {
-        // Rotate Rover
-        if (this.direction === "N") {
-          if (command === "r") {
-            this.direction = "E";
-          } else {
-            this.direction = "W";
-          }
-        } else if (this.direction === "S") {
-          if (command === "r") {
-            this.direction = "W";
-          } else {
-            this.direction = "E";
-          }
-        } else if (this.direction === "W") {
-          if (command === "r") {
-            this.direction = "N";
-          } else {
-            this.direction = "S";
-          }
+      this.process(command);
+    }
+  }
+
+  private process(command: string) {
+    if (command === "l" || command === "r") {
+      // Rotate Rover
+      if (this.direction === "N") {
+        if (command === "r") {
+          this.direction = "E";
         } else {
-          if (command === "r") {
-            this.direction = "S";
-          } else {
-            this.direction = "N";
-          }
+          this.direction = "W";
+        }
+      } else if (this.direction === "S") {
+        if (command === "r") {
+          this.direction = "W";
+        } else {
+          this.direction = "E";
+        }
+      } else if (this.direction === "W") {
+        if (command === "r") {
+          this.direction = "N";
+        } else {
+          this.direction = "S";
         }
       } else {
-        // Displace Rover
-        let displacement1 = -1;
-
-        if (command === "f") {
-          displacement1 = 1;
-        }
-        let displacement = displacement1;
-
-        if (this.direction === "N") {
-          this.y += displacement;
-        } else if (this.direction === "S") {
-          this.y -= displacement;
-        } else if (this.direction === "W") {
-          this.x -= displacement;
+        if (command === "r") {
+          this.direction = "S";
         } else {
-          this.x += displacement;
+          this.direction = "N";
         }
+      }
+    } else if (command === "l" || command === "r") {
+      // Rotate Rover
+      if (this.direction === "N") {
+        if (command === "r") {
+          this.direction = "E";
+        } else {
+          this.direction = "W";
+        }
+      } else if (this.direction === "S") {
+        if (command === "r") {
+          this.direction = "W";
+        } else {
+          this.direction = "E";
+        }
+      } else if (this.direction === "W") {
+        if (command === "r") {
+          this.direction = "N";
+        } else {
+          this.direction = "S";
+        }
+      } else {
+        if (command === "r") {
+          this.direction = "S";
+        } else {
+          this.direction = "N";
+        }
+      }
+    } else {
+      // Displace Rover
+      let displacement1 = -1;
+
+      if (command === "f") {
+        displacement1 = 1;
+      }
+      let displacement = displacement1;
+
+      if (this.direction === "N") {
+        this.y += displacement;
+      } else if (this.direction === "S") {
+        this.y -= displacement;
+      } else if (this.direction === "W") {
+        this.x -= displacement;
+      } else {
+        this.x += displacement;
       }
     }
   }
